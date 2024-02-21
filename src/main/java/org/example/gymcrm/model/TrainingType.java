b7856ce45e08f2b9819ee27d68e858d81edfef5d
EternityRei <anastasiamayorova2003@gmail.com>
2024-02-21 22:07:39 +0200

    Change TrainingType class type

--- src/main/java/org/example/gymcrm/model/TrainingType.java
+++ src/main/java/org/example/gymcrm/model/TrainingType.java
@@ -1,5 +1,31 @@
 package org.example.gymcrm.model;
 
-public enum TrainingType {
-    PASS
+public class TrainingType {
+    private String id;
+    private String name;
+
+    public TrainingType(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public TrainingType(){
+
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
