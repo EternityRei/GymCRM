c8f0af932aea118e6993432aa864314127bd5476
EternityRei <anastasiamayorova2003@gmail.com>
2024-02-24 11:15:09 +0200

    Add some main interactions

--- src/main/java/org/example/gymcrm/GymCrmApplication.java
+++ src/main/java/org/example/gymcrm/GymCrmApplication.java
@@ -13,38 +13,68 @@ import org.example.gymcrm.service.impl.TrainerServiceImpl;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.List;
 import java.util.UUID;
 
 public class GymCrmApplication {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        Trainee trainee = new Trainee();
-        trainee.setId(String.valueOf(UUID.randomUUID()));
-        trainee.setFirstName("Joe");
-        trainee.setLastName("Doe");
+        Trainee joeDoe = new Trainee();
+        joeDoe.setId(String.valueOf(UUID.randomUUID()));
+        joeDoe.setFirstName("Joe");
+        joeDoe.setLastName("Doe");
 
-        Trainee anotherTrainee = new Trainee();
-        anotherTrainee.setId(String.valueOf(UUID.randomUUID()));
-        anotherTrainee.setFirstName("Joe");
-        anotherTrainee.setLastName("Doe");
+        Trainee joeDoe1 = new Trainee();
+        joeDoe1.setId(String.valueOf(UUID.randomUUID()));
+        joeDoe1.setFirstName("Joe");
+        joeDoe1.setLastName("Doe");
 
-        Trainer trainer = new Trainer();
-        trainer.setId(String.valueOf(UUID.randomUUID()));
-        trainer.setFirstName("Joe");
-        trainer.setLastName("Doe");
+        Trainer joeDoe2trainer = new Trainer();
+        joeDoe2trainer.setId(String.valueOf(UUID.randomUUID()));
+        joeDoe2trainer.setFirstName("Joe");
+        joeDoe2trainer.setLastName("Doe");
 
         TraineeServiceImpl traineeService = context.getBean("traineeServiceImpl", TraineeServiceImpl.class);
         TrainerServiceImpl trainerService = context.getBean("trainerServiceImpl", TrainerServiceImpl.class);
 
-        Trainee createdTrainee = traineeService.createTrainee(trainee);
-        Trainee createdTrainee2 = traineeService.createTrainee(anotherTrainee);
+        Trainee createdTrainee = traineeService.createTrainee(joeDoe);
+        Trainee createdTrainee2 = traineeService.createTrainee(joeDoe1);
 
-        Trainer createdTrainer = trainerService.createTrainer(trainer);
+        List<Trainee> allTrainees = traineeService.getAllTrainees();
+        System.out.println("All Trainees: " + allTrainees);
+
+        Trainer createdTrainer = trainerService.createTrainer(joeDoe2trainer);
 
         System.out.println("Created Trainee1: " + createdTrainee);
         System.out.println("Created Trainee2: " + createdTrainee2);
 
         System.out.println("Created Trainer: " + createdTrainer);
+
+        traineeService.deleteTrainee(joeDoe1.getId());
+
+        Trainee joeDoeError = new Trainee();
+        joeDoeError.setId(String.valueOf(UUID.randomUUID()));
+        joeDoeError.setFirstName("Joe");
+        joeDoeError.setLastName("Doe");
+
+        System.out.println("Created Trainee3: " + createdTrainee2);
+
+
+        System.out.println("All Trainees: " + allTrainees);
+
+        traineeService.deleteTrainee(joeDoe.getId());
+
+        System.out.println("All Trainees after deletion: " + allTrainees);
+
+        Trainee joeDoe3 = new Trainee();
+        joeDoe3.setId(String.valueOf(UUID.randomUUID()));
+        joeDoe3.setFirstName("Joe");
+        joeDoe3.setLastName("Doe");
+
+        Trainee joeDoe3Created = traineeService.createTrainee(joeDoe3);
+
+        System.out.println("Created Trainee4: " + joeDoe3Created);
+
     }
 }
